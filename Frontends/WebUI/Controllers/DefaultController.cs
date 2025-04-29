0d024492b49257c7e050957b3f45e869a26f4a34
CdevWizards <ubahmet32@hotmail.com>
2025-04-29 17:53:49 +0300

    RenACarListSide

--- Frontends/WebUI/Controllers/DefaultController.cs
+++ Frontends/WebUI/Controllers/DefaultController.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using UdemyCarBook.Dto.LocationDtos;
+using Newtonsoft.Json;
+
 
 namespace WebUI.Controllers
 {
@@ -20,24 +21,37 @@ namespace WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage=await client.GetAsync("http://localhost:5204/api/Location");
-            if(responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values=JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
-                List<SelectListItem> values2=(from x in values
-                select new SelectListItem
-                {
-                    Text = x.Name,
-                    Value = x.LocationID.ToString()
-                }).ToList();
-                ViewBag.v=values2;
+        [HttpGet]
+public async Task<IActionResult> Index()
+{
+    var client = _httpClientFactory.CreateClient();
+    var responseMessage = await client.GetAsync("http://localhost:5204/api/Location");
+
+    if(responseMessage.IsSuccessStatusCode)
+    {
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                List<SelectListItem> values2 = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.LocationID.ToString()
+                                                }).ToList();
+                ViewBag.v = values2;
             }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string book_pick_date, string book_off_date,string time_pick,string time_off,string locationID)
+        {
+            TempData["timeoff"] = time_off;
+            TempData["timepick"] = time_pick;
+            TempData["locationID"] = locationID;
+            TempData["bookpickdate"] = book_pick_date;
+            TempData["bookoffdate"] = book_off_date;
+            return RedirectToAction("Index", "RentACarList");
+        }
 
     }
-    }
-    }
+}
--- Frontends/WebUI/Controllers/RentACarListController.cs
+++ Frontends/WebUI/Controllers/RentACarListController.cs
@@ -2,18 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.RentACarDtos;
 
 namespace WebUI.Controllers
 {
     public class RentACarListController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RentACarListController(IHttpClientFactory httpClientFactory)
         {
-            var data = TempData["value"];
-            ViewBag.v=data;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(FilterRentACarDto filterRentACarDto)
+        {
+            var timeoff = TempData["timeoff"];
+            var timepick = TempData["timepick"];
+            var locationID = TempData["locationID"];
+            var bookpickdate = TempData["bookpickdate"];
+            var bookoffdate = TempData["bookoffdate"];
+
+            filterRentACarDto.locationID = int.Parse(locationID.ToString());
+            filterRentACarDto.available = true;
+            
+            ViewBag.bookpickdate=bookpickdate;
+            ViewBag.bookoffdate=bookoffdate;
+            ViewBag.timeoff=timeoff;
+            ViewBag.locationID=locationID;
+            ViewBag.timepick=timepick;
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(locationID);
+            StringContent stringContent = new StringContent (jsonData, Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5204/api/RentACars", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
